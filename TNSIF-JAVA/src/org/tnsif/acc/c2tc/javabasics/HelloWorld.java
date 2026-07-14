17e4d7809444a6ba32f375a5b5f344f7d9fe4d41
sriva <sriva@sri>
2026-07-14 10:15:19 +0530

    first commit

---
+++ TNSIF-JAVA/src/org/tnsif/acc/c2tc/javabasics/HelloWorld.java
@@ -0,0 +1,11 @@
+package org.tnsif.acc.c2tc.javabasics;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
