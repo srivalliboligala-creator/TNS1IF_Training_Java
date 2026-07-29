47ae0a9ee18d79739b399cf5492788ff5b4ecdb9
sriva <sriva@sri>
2026-07-29 10:07:46 +0530

    first commit

---
+++ TNSIF-JAVA/src/org/tnsif/acc/c2tc/methodoverriding/SuperWithVariable.java
@@ -0,0 +1,24 @@
+package org.tnsif.acc.c2tc.methodoverriding;
+class Notification
+{
+	 String message=" Default Notification";
+}
+class PushNotification extends Notification
+{
+	String message="Push Notification";
+	
+	void showNotification()
+	{
+		System.out.println("Child message :" +message);
+		System.out.println("Parent message : "+super.message);
+	}
+}
+public class SuperWithVariable {
+
+	public static void main(String[] args) {
+		 PushNotification obj=new  PushNotification();
+		 obj.showNotification();
+
+	}
+
+}
